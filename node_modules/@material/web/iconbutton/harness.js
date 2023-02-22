2a0b89dde6d8d260b75c4d8ca5e844d3ba8bf741
zach-anam <zach.anam@gmail.com>
2023-02-22 06:36:28 +0300

    intilize commet

---
+++ node_modules/@material/web/iconbutton/harness.js
@@ -0,0 +1,16 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { Harness } from '../testing/harness.js';
+/**
+ * Test harness for icon buttons.
+ */
+export class IconButtonHarness extends Harness {
+    async getInteractiveElement() {
+        await this.element.updateComplete;
+        return this.element.renderRoot.querySelector('.md3-icon-button');
+    }
+}
+//# sourceMappingURL=harness.js.map
